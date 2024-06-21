1c7d7edbb57da8ac973052863de37f7586b3fcfc
KrankerSchrank <kranker.schrank@posteo.de>
2024-06-22 01:15:44 +0200

    Inventar System Berufe, Monster, Items sowie Schlüssel Hinzugefügt Aktionen Suchen, 7. Sinn und Bannen hinzugefügt

--- lib/src/logic/game_logic.dart
+++ lib/src/logic/game_logic.dart
@@ -1,14 +1,25 @@
+import 'dart:math' show Random;
+
 class Game {
   // Variables to store game state
   bool isInitialized = false;
   bool isGameOver = false;
   int lastPressedButtonIndex = 99;
   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  var animalNames = ['cat', 'dog', 'elephant', 'lion', 'tiger', 'giraffe', 'monkey', 'zebra', 'penguin', 'koala'];
+  var besessene = ['Wirt', 'Bäcker', 'Metzger', 'Bauer', 'Fischer', 'Schmied', 'Schneider', 'Krämer', 'Stadtwächter', 'Bürgermeister'];
+  var monster = ['Bogart', 'Würmlinge', 'Skelts', 'Wichte', 'Flusshexe', 'Feuergeist', 'Mänade', 'Erdgeist', 'Sirene', 'Laminahexe'];
+  var bannItems = ['Salz', 'Eisen', 'Silberkette', 'Stab', 'Messer', 'Schere', 'Gold', 'Lampe', 'Amulett', 'Kreuz'];
+  var bannPosition = ['', '', '', '', '', '', '', '', '', ''];
+  var bannStatus = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var items = ['Universalschlüssel', 'Salz', 'Eisen', 'Silberkette', 'Stab', 'Messer', 'Schere', 'Gold', 'Lampe', 'Amulett', 'Kreuz'];
+  var inventory = [];
+  var schluessel = 0;
+  var position = ['', '', '', '', '', '', '', '', '', ''];
+  var used = [];
+  var item = '';
 
   // Function to initialize the game
   Game() {
-    // TODO: Add initialization logic here
     isInitialized = true;
   }
 
@@ -22,31 +33,78 @@ class Game {
       throw Exception('Game over!');
     }
 
-    // TODO: Add button press logic here
     if (pressedButtonIndex < 10) {
       lastPressedButtonIndex = pressedButtonIndex;
-      return 'Button ${animalNames[pressedButtonIndex]} pressed!';
+      return 'Button ${besessene[pressedButtonIndex]} pressed!';
     }
     else if (lastPressedButtonIndex < 10) {
       String buttonText = '';
       switch (pressedButtonIndex) {
         case 10:
-          buttonText = 'Animal ${animalNames[lastPressedButtonIndex]} goes to the zoo!';
+          if (position[lastPressedButtonIndex] == '') {
+            int random = Random().nextInt(items.length);
+            if (items[random] == 'Universalschlüssel') {
+              schluessel += 1;
+              item = 'Universalschlüssel';
+            }
+            else {
+              position[lastPressedButtonIndex] = items[random];
+              item = position[lastPressedButtonIndex];
+              if (random != 0) {
+                items.removeAt(random);
+              }
+            }
+          }
+          else if (Random().nextInt(5) == 1) {
+            item = 'Universalschlüssel';
+            schluessel += 1;
+          }
+          else {
+            item = position[lastPressedButtonIndex];
+          }
+          if (inventory.contains(item) || used.contains(item)) {
+            buttonText = 'Hier gibt es nichts zu finden!';
+          }
+          else {
+            buttonText = 'Du hast $item gefunden!';
+            inventory.add(item);
+          }
           break;
         case 11:
-          buttonText = 'Animal ${animalNames[lastPressedButtonIndex]} enjoys some playtime!';
+          if (bannPosition[lastPressedButtonIndex] == '') {
+            int random = Random().nextInt(bannItems.length);
+            bannPosition[lastPressedButtonIndex] = bannItems[random];
+            item = bannPosition[lastPressedButtonIndex];
+            bannItems.removeAt(random);
+          }
+          else {
+            item = bannPosition[lastPressedButtonIndex];
+          }
+          buttonText = 'Du spürst das du ${monster[lastPressedButtonIndex]} mit $item bannen kannst!';
           break;
           case 12:
-            buttonText = 'Animal ${animalNames[lastPressedButtonIndex]} takes a nap!';
+            if (bannStatus[lastPressedButtonIndex] == 1) {
+              buttonText = 'Du hast ${monster[lastPressedButtonIndex]} bereits gebannt!';
+            }
+            else {
+              if (bannPosition[lastPressedButtonIndex] == '') {
+                buttonText = 'Du weißt nicht mit was du ${monster[lastPressedButtonIndex]} bannen sollst!';
+              }
+              else if (inventory.contains(bannPosition[lastPressedButtonIndex])) {
+                buttonText = 'Du hast ${monster[lastPressedButtonIndex]} mit ${bannPosition[lastPressedButtonIndex]} gebannt, Der Verräter ist ...!';
+                used.add(bannPosition[lastPressedButtonIndex]);
+                inventory.remove(bannPosition[lastPressedButtonIndex]);
+                bannStatus[lastPressedButtonIndex] = 1;
+              }
+            }
             break;
           case 13:
-            buttonText = 'Animal ${animalNames[lastPressedButtonIndex]} eats some food!';
+          // TODO: Implement Zaubern button
+            buttonText = 'Animal ${besessene[lastPressedButtonIndex]} eats some food!';
             break;
           case 14:
-            buttonText = 'Animal ${animalNames[lastPressedButtonIndex]} plays with a toy!';
-            break;
-          case 15:
-            buttonText = 'Animal ${animalNames[lastPressedButtonIndex]} goes for a walk!';
+          // TODO: Implement Aufbrechen button
+            buttonText = 'Animal ${besessene[lastPressedButtonIndex]} plays with a toy!';
             break;
         default:
           return 'Error processing button press!';
--- lib/src/sample_feature/button_grid.dart
+++ lib/src/sample_feature/button_grid.dart
@@ -15,14 +15,14 @@ class ButtonGrid extends StatelessWidget {
         crossAxisCount: 5,
         childAspectRatio: 1.7,
         children: List.generate(15, (index) {
-          var animalNames = ['cat', 'dog', 'elephant', 'lion', 'tiger', 'giraffe', 'monkey', 'zebra', 'penguin', 'koala'];
+          var besessene = ['Wirt', 'Bäcker', 'Metzger', 'Bauer', 'Fischer', 'Schmied', 'Schneider', 'Krämer', 'Stadtwächter', 'Bürgermeister'];
           return OutlinedButton(
             onPressed: () {
               // Handle button press
               showLoaderDialog(context, index, game);
             },
             child: Text(
-              index < 10 ? animalNames[index] : index == 10 ? 'Zoo' : index == 11 ? 'Playtime' : index == 12 ? 'Nap' : index == 13 ? 'Food' : index == 14 ? 'Toy' : 'Walk',
+              index < 10 ? besessene[index] : index == 10 ? 'Suchen' : index == 11 ? '7. Sinn' : index == 12 ? 'Bannen' : index == 13 ? 'Zaubern' :  'Aufbrechen',
               style: const TextStyle(fontSize: 16.0),
             ),
           );
@@ -34,29 +34,31 @@ class ButtonGrid extends StatelessWidget {
 
 showLoaderDialog(BuildContext context, buttonInfo, game) {
   String buttonText = game.processButtonPress(buttonInfo);
-    AlertDialog alert = AlertDialog(
-      content: Row(
-        children: [
-          Container(
-              margin: const EdgeInsets.only(left: 7), child: Text(buttonText)),
-              Container(
-                margin: const EdgeInsets.only(left: 70, right: 7),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Handle OK button press
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('OK'),
+    if (buttonInfo >= 10) {
+      AlertDialog alert = AlertDialog(
+        content: Row(
+          children: [
+            Container(
+                margin: const EdgeInsets.only(left: 7), child: Text(buttonText)),
+                Container(
+                  margin: const EdgeInsets.only(left: 70, right: 7),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle OK button press
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('OK'),
+                  ),
                 ),
-              ),
-        ],
-      ),
-    );
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
+          ],
+        ),
+      );
+      showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+    }
   }
